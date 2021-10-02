c2d30ca6c47c003fc388d5f598bdfe245641d75c
artalatarta <artur@mucowski.pl>
2021-10-02 12:22:39 +0200

    QR-11: added basic text input

--- src/app/rock/routes/codes/Hacker.vue
+++ src/app/rock/routes/codes/Hacker.vue
@@ -1,17 +1,26 @@
 <template>
 	<p>Haker</p>
 	<Video url="https://www.youtube.com/embed/4-fOGS_QcZk" />
+	<Input label="hasło" name="hackerPasswordInput" @submitInput="submit($event)" />
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 
+	import Input from '@/components/Input.vue';
 	import Video from '@/components/Video.vue';
 
 	export default defineComponent({
 		name: 'Hacker',
 		components: {
+			Input,
 			Video,
+		},
+		setup() {
+			function submit(e: string): void {
+				console.log(e);
+			}
+			return { submit };
 		},
 	});
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+	<label :for="name">{{ label }}</label>
+	<input v-model="input" :name="name" type="text" autocomplete="off" @keydown.enter="$emit('submitInput', input)" />
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'Input',
+		props: {
+			name: {
+				type: String,
+				required: true,
+			},
+			label: {
+				type: String,
+				required: true,
+			},
+			event: {
+				type: String,
+				required: true,
+			},
+		},
+		emits: ['submitInput'],
+		setup() {
+			let input = '';
+			return { input };
+		},
+	});
+</script>
+<style lang="scss" scoped>
+</style>
