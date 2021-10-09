cec098e26e50af39f5720a282e1ae20335816d2f
artalatarta <artur@mucowski.pl>
2021-10-09 13:04:53 +0200

    QR-25: single question refactor

--- src/app/rock/routes/codes/Inspector.vue
+++ src/app/rock/routes/codes/Inspector.vue
@@ -1,17 +1,36 @@
 <template>
 	<p>Inspektor</p>
 	<Video url="https://www.youtube.com/embed/4-fOGS_QcZk" />
+	<SingleQuestion
+		:options="[
+			'https://images.pexels.com/photos/3823495/pexels-photo-3823495.jpeg',
+			'https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg',
+			'https://images.pexels.com/photos/2389349/pexels-photo-2389349.jpeg',
+			'https://images.pexels.com/photos/4655426/pexels-photo-4655426.jpeg',
+			'https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg',
+		]"
+		name="causer"
+		@answer="log($event)"
+	/>
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 
 	import Video from '@/components/Video.vue';
+	import SingleQuestion from '@/components/closedQuestion/SingleQuestion.vue';
 
 	export default defineComponent({
 		name: 'Inspector',
 		components: {
 			Video,
+			SingleQuestion,
+		},
+		setup() {
+			function log(e: string): void {
+				console.log(e);
+			}
+			return { log };
 		},
 	});
 </script>
---
+++ src/components/closedQuestion/SingleQuestion.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="container">
+		<div v-for="(option, i) in options" :key="`option${i}`">
+			<input :id="`${name}Option${i}`" :name="name" :value="i" type="radio" @click="$emit('answer', $event.target.value)" />
+			<label :for="`${name}Option${i}`">
+				<img v-if="arePhotos" :src="option" />
+				<span v-else>{{ option }}</span>
+			</label>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent, PropType } from 'vue';
+
+	import { arePhotos } from './arePhotos';
+	import { hideInput } from './hideInput';
+
+	export default defineComponent({
+		name: 'SingleQuestion',
+		props: {
+			name: {
+				type: String,
+				required: true,
+			},
+			options: {
+				type: Array as PropType<string[]>,
+				required: true,
+			},
+		},
+		emits: ['answer'],
+		setup(props) {
+			return { arePhotos: arePhotos(props.options), hideInput };
+		},
+	});
+</script>
+<style lang="scss" scoped >
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		input {
+			visibility: v-bind('hideInput(arePhotos)');
+		}
+		img {
+			width: 150px;
+		}
+	}
+</style>
