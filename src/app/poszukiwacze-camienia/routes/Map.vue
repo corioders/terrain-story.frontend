8859376d75aac83b9d207a3ae13640e25575e4e0
artalatarta <artur@mucowski.pl>
2021-10-21 17:01:05 +0200

    QR-61: add map component

--- src/app/poszukiwacze-camienia/routes/Map.vue
+++ src/app/poszukiwacze-camienia/routes/Map.vue
@@ -1,12 +1,15 @@
-<template>
-	<p>Mapa</p>
-</template>
+<template><GMap /></template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 
+	import GMap from '@/components/map/GMap.vue';
+
 	export default defineComponent({
 		name: 'Map',
+		components: {
+			GMap,
+		},
 	});
 </script>
 <style lang="scss" scoped>
---
+++ src/components/map/GMap.vue
@@ -0,0 +1,23 @@
+<template>
+	<iframe
+		width="600"
+		height="500"
+		style="margin-top: 24px"
+		src="https://maps.google.com/maps?q=Ko%C5%9Bci%C3%B3%C5%82%20Mariacki,%20Katowice&t=&z=15&ie=UTF8&iwloc=&output=embed"
+		frameborder="0"
+		scrolling="no"
+		marginheight="0"
+		marginwidth="0"
+	></iframe>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'GMap',
+		setup() {
+			return {};
+		},
+	});
+</script>
