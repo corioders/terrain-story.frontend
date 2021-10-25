0d6624fc5a8442e2d211b97bb40a2531de330b00
artalatarta <artur@mucowski.pl>
2021-10-25 13:51:16 +0200

    QR-131: add witnesses

--- src/app/poszukiwacze-camienia/routes/codes/Inspector.vue
+++ src/app/poszukiwacze-camienia/routes/codes/Inspector.vue
@@ -1,25 +1,48 @@
 <template>
 	<p>Inspektor</p>
 	<Video url="https://www.youtube.com/embed/4-fOGS_QcZk" />
+	<n-card title="Zeznania świadków" class="witnesses">
+		<n-space vertical>
+			<n-card v-for="(witness, i) in witnesses" :key="`Witness${i + 1}`" :title="`Zeznanie ${i + 1} świadka`">
+				{{ witness }}
+			</n-card>
+		</n-space>
+	</n-card>
 	<SingleChoiceQuestion :options="question.options" :answer="question.answer" disableMixing name="causer" />
 </template>
 
 <script lang="ts">
+	import { NCard, NSpace } from 'naive-ui';
 	import { defineComponent } from 'vue';
 
 	import Video from '@/components/Video.vue';
 	import SingleChoiceQuestion from '@/components/closedQuestion/SingleChoiceQuestion.vue';
 
 	import { inspector as question } from '@rock/static/questions';
+	import witnesses from '@rock/static/witnesses';
 
 	export default defineComponent({
 		name: 'Inspector',
 		components: {
+			NCard,
+			NSpace,
 			Video,
 			SingleChoiceQuestion,
 		},
 		setup() {
-			return { question };
+			return { question, witnesses };
 		},
 	});
 </script>
+
+<style lang="scss" scoped>
+	.witnesses {
+		width: 95%;
+		max-width: 950px;
+		.n-space {
+			.n-card {
+				width: 100%;
+			}
+		}
+	}
+</style>
