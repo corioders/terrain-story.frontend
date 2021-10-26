40085d5c2864270a3f5d52bf8c2c6addda91b1ab
artalatarta <artur@mucowski.pl>
2021-10-26 17:08:42 +0200

    QR-140: Add photos to tourist

--- src/app/poszukiwacze-camienia/routes/codes/Tourist.vue
+++ src/app/poszukiwacze-camienia/routes/codes/Tourist.vue
@@ -1,7 +1,20 @@
 <template>
 	<p>Turysta</p>
 	<Video url="https://www.youtube.com/embed/HcVCHKHy-Xw" />
-	<SingleChoiceQuestions :questions="questions" name="place" />
+	<SingleChoiceQuestions :questions="questions" name="place">
+		<template #0>
+			<img src="@rock/assets/places/0.jpg" alt="Miejsce 1" />
+		</template>
+		<template #1>
+			<img src="@rock/assets/places/1.jpg" alt="Miejsce 2" />
+		</template>
+		<template #2>
+			<img src="@rock/assets/places/2.jpg" alt="Miejsce 3" />
+		</template>
+		<template #3>
+			<img src="@rock/assets/places/3.jpg" alt="Miejsce 4" />
+		</template>
+	</SingleChoiceQuestions>
 </template>
 
 <script lang="ts">
