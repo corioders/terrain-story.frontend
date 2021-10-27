6c3285bb37358c3e0a6dc1bcb5f5c70b51424284
watjurk <watjurk@gmail.com>
2021-10-27 16:30:50 +0200

    QR-63: Add trapezoid coloring

---
+++ src/app/poszukiwacze-camienia/components/trapezoidColoring/TrapezoidColoring.vue
@@ -0,0 +1,154 @@
+<template>
+	<div class="container">
+		<n-scrollbar x-scrollable>
+			<div class="columnLabelContainer">
+				<div class="filler"></div>
+				<div v-for="column in COLUMNS" :key="`columnLabel${column}`" class="columnLabel">
+					<p>{{ alphabet[column - 1] }}</p>
+				</div>
+			</div>
+			<div class="trapezoidColoring">
+				<div class="rowLabelContainer">
+					<div v-for="row in ROWS" :key="`rowLabel${row}`" class="rowLabel">
+						<p>{{ row }}</p>
+					</div>
+				</div>
+				<div v-for="column in COLUMNS" :key="`column${column}`" class="column">
+					<div
+						v-for="row in ROWS"
+						:key="`row${row}`"
+						class="box"
+						@click="handleClick($event.target.classList, column, row)"
+						@tap="handleClick($event.target.classList, column, row)"
+					></div>
+				</div>
+			</div>
+		</n-scrollbar>
+	</div>
+</template>
+
+<script lang="ts">
+	import { NScrollbar } from 'naive-ui';
+	import { defineComponent, PropType } from 'vue';
+
+	import { alphabetUpper } from '@rock/assets/alphabet';
+
+	import { TrapezoidDescriptor } from './trapezoid';
+
+	export default defineComponent({
+		name: 'TrapezoidColoring',
+		components: {
+			// NScrollbar,
+		},
+		props: {
+			trapezoidDescriptor: {
+				type: Object as PropType<TrapezoidDescriptor>,
+				required: true,
+			},
+		},
+		emits: ['correct', 'incorrect'],
+		setup(props, { emit }) {
+			const correctColorsMap = new Map<string, boolean>();
+			for (const color of props.trapezoidDescriptor.correctColors) {
+				const key = getKey(color.x, color.y);
+				correctColorsMap.set(key, true);
+			}
+			const colorsMap = new Map<string, boolean>();
+
+			function handleClick(classList: DOMTokenList, column: number, row: number): void {
+				classList.toggle('checked');
+				const x = column - 1;
+				const y = row - 1;
+
+				const key = getKey(x, y);
+				if (!colorsMap.has(key)) {
+					colorsMap.set(key, true);
+				} else {
+					const value = colorsMap.get(key);
+					colorsMap.set(key, !value);
+				}
+
+				// Check correct answer.
+				for (const [key, value] of colorsMap) {
+					if (value === true && !correctColorsMap.has(key)) {
+						console.log('Wrong check', key);
+						emit('incorrect');
+						return;
+					}
+				}
+				for (const key of correctColorsMap.keys()) {
+					if (!colorsMap.has(key)) {
+						console.log('Not checked');
+						emit('incorrect');
+						return;
+					}
+					if (colorsMap.get(key) === false) {
+						console.log('Not checked');
+						emit('incorrect');
+						return;
+					}
+				}
+
+				emit('correct');
+			}
+
+			return { alphabet: alphabetUpper, handleClick, COLUMNS: props.trapezoidDescriptor.dimensions.width, ROWS: props.trapezoidDescriptor.dimensions.height };
+		},
+	});
+
+	function getKey(x: number, y: number): string {
+		return `${x}.${y}`;
+	}
+</script>
+<style lang="scss" scoped>
+	.trapezoidColoring {
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: row;
+		border: 1px #000 solid;
+	}
+	.rowLabelContainer {
+		display: flex;
+		flex-direction: column;
+		border-right: 1px #000 solid;
+	}
+	.columnLabelContainer {
+		display: flex;
+		flex-direction: row;
+		border: 1px #000 solid;
+		border-bottom: none;
+	}
+	.columnLabelContainer,
+	.rowLabelContainer {
+		p {
+			text-align: center;
+		}
+	}
+	.box,
+	.rowLabel,
+	.columnLabel {
+		width: 50px;
+		height: 50px;
+		border: 1px #000 solid;
+	}
+	.filler {
+		width: 50px;
+		width: 51px;
+		border: 1px #000 solid;
+		border-right: 2px #000 solid;
+	}
+	.checked {
+		background-color: $primary;
+	}
+	.column {
+		display: flex;
+		flex-direction: column;
+	}
+	.container {
+		max-width: 95%;
+		height: 300px;
+		@media (min-width: 1000px) {
+			max-width: 950px;
+		}
+	}
+</style>
--- src/app/poszukiwacze-camienia/routes/codes/Archaeologist.vue
+++ src/app/poszukiwacze-camienia/routes/codes/Archaeologist.vue
@@ -5,15 +5,15 @@
 		<p>
 			winszuje inicjatywy. Cieszę się, że razem będziemy mogli ruszyć na poszukiwania tego niezwykłego artefaktu. Prawdę powiedziawszy sam, poczyniłem już pierwsze kroki.
 			W środku Wenezuelskiej dżungli, udało mi się odnaleźć starożytne ruiny majów, które kierują do lokalizacji Camienia. Niestety Doktor Zło czai się na każdym kroku.
-			Wysyłam je Wam w zakodowanej formie. Nanieście punkty na przecięcie linii, a następnie przeprowadźcie linie z przeciwległych wierzchołków uzyskanej figury.
-			Wskazówki znajdują się w miejscu przecięcia tych linii.
+			Wysyłam je Wam w zakodowanej formie. Nanieście poniższe koordynaty na pole, wtedy ukaże wam się wskazówka.
 		</p>
 		<h4>
 			Powodzenia <br />
 			Wasz Scott
 		</h4>
+    <p>E8, K4, H5, R8, I4, F8, P7, O6, N5, K8, G6, I8, N8, O8, J8, L4, L8, S8, F7, P8, M4, G8, H8, M8, J4, D8</p>
 	</n-card>
-	<TrapezoidColoring />
+	<TrapezoidColoring :trapezoidDescriptor="trapezoidDescriptor" @correct="console.log('correct')" @incorrect="console.log('incorrect')" />
 </template>
 
 <script lang="ts">
@@ -22,7 +22,7 @@
 
 	import Video from '@/components/Video.vue';
 
-	import TrapezoidColoring from '@rock/components/TrapezoidColoring.vue';
+	import TrapezoidColoring from '@/app/poszukiwacze-camienia/components/trapezoidColoring/TrapezoidColoring.vue';
 
 	export default defineComponent({
 		name: 'Archaeologist',
@@ -31,6 +31,48 @@
 			Video,
 			TrapezoidColoring,
 		},
+		setup() {
+			return { console };
+		},
+		data: () => ({
+			trapezoidDescriptor: {
+				dimensions: {
+					width: 18,
+					height: 15,
+				},
+				correctColors: [
+					{ x: 3, y: 7 },
+					{ x: 4, y: 7 },
+					{ x: 5, y: 7 },
+					{ x: 6, y: 7 },
+					{ x: 7, y: 7 },
+					{ x: 8, y: 7 },
+					{ x: 9, y: 7 },
+					{ x: 10, y: 7 },
+					{ x: 11, y: 7 },
+					{ x: 12, y: 7 },
+					{ x: 13, y: 7 },
+					{ x: 14, y: 7 },
+					{ x: 15, y: 7 },
+					{ x: 16, y: 7 },
+					{ x: 17, y: 7 },
+
+					{ x: 15, y: 6 },
+					{ x: 14, y: 5 },
+					{ x: 13, y: 4 },
+
+					{ x: 12, y: 3 },
+					{ x: 11, y: 3 },
+					{ x: 10, y: 3 },
+					{ x: 9, y: 3 },
+					{ x: 8, y: 3 },
+
+					{ x: 7, y: 4 },
+					{ x: 6, y: 5 },
+					{ x: 5, y: 6 },
+				],
+			},
+		}),
 	});
 </script>
 <style lang="scss" scoped>
