fe9845a89d83ae24bebe089ae9fe48ba4ab08542
artalatarta <artur@mucowski.pl>
2021-10-27 12:14:31 +0200

    QR-172: Style PlayButton

--- src/app/poszukiwacze-camienia/views/Start.vue
+++ src/app/poszukiwacze-camienia/views/Start.vue
@@ -1,13 +1,14 @@
 <template>
 	<p>Start-Kosmita</p>
 	<Video url="https://www.youtube.com/embed/q_O3LVHoFAw" />
-	<button @click="handleStart()">OK</button>
+	<PlayButton @click="handleStart()" />
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 	import { useRouter } from 'vue-router';
 
+	import PlayButton from '@/components/PlayButton.vue';
 	import Video from '@/components/Video.vue';
 
 	import { useProgressStore } from '@rock/store/progress';
@@ -15,6 +16,7 @@
 	export default defineComponent({
 		name: 'Start',
 		components: {
+			PlayButton,
 			Video,
 		},
 		setup() {
---
+++ src/components/PlayButton.vue
@@ -0,0 +1,16 @@
+<template>
+	<n-button size="large" type="primary" @click="$emit('click')">Zagraj</n-button>
+</template>
+
+<script lang="ts">
+	import { NButton } from 'naive-ui';
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'PlayButton',
+		components: {
+			NButton,
+		},
+		emits: ['click'],
+	});
+</script>
