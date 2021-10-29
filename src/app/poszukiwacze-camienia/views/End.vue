7b0b5e6bab6185db14c2facb3a840bec0452b7e7
artalatarta <artur@mucowski.pl>
2021-10-29 11:37:59 +0200

    QR-213: Add reset progress button in End route

--- src/app/poszukiwacze-camienia/views/End.vue
+++ src/app/poszukiwacze-camienia/views/End.vue
@@ -1,16 +1,19 @@
 <template>
 	<p>Koniec-Kosmita</p>
 	<Video url="https://www.youtube.com/embed/PIJysna39aM" />
+	<ResetProgressButton />
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 
 	import Video from '@/components/Video.vue';
+	import ResetProgressButton from '@/components/buttons/ResetProgressButton.vue';
 
 	export default defineComponent({
 		name: 'End',
 		components: {
+			ResetProgressButton,
 			Video,
 		},
 	});
