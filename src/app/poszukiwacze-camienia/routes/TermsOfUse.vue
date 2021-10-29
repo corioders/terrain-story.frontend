badf7aba7acb0da1904ceaf54a1d59a29d376318
artalatarta <artur@mucowski.pl>
2021-10-29 22:52:11 +0200

    QR-225: Add TermsOfUse

--- src/app/poszukiwacze-camienia/routes/TermsOfUse.vue
+++ src/app/poszukiwacze-camienia/routes/TermsOfUse.vue
@@ -1,6 +1,149 @@
 <template>
 	<n-space vertical align="center">
-		<h1>Regulamin</h1>
+		<article>
+			<p class="Heading">Regulamin gry</p>
+			<p>„ <strong>Poszukiwacze Camienia&quot;</strong></p>
+			<p><strong>Informacje ogólne</strong></p>
+			<ol>
+				<li>Gra terenowa „Poszukiwacze Camienia&quot;, zwana dalej Grą, swoją premierę będzie miała w dniu 05.11.2021r. (piątek).</li>
+				<li>Organizatorami Gry jest: Centrum Aktywności Młodzieżowej ul. Kopernika 14, Stowarzyszenie Most wraz ze współpracą z Urzędem Miejskim w Katowicach.</li>
+				<li>
+					Regulamin jest dostępny w siedzibie Centrum Aktywności Młodzieżowej oraz na facebook
+					<a href="https://www.facebook.com/CentrumAktywnosciMlodziezowej">https://www.facebook.com/CentrumAktywnosciMlodziezowej</a>
+				</li>
+			</ol>
+			<p><strong>Zasady gry</strong></p>
+			<ol>
+				<li>
+					Przez organizację Gry rozumie się przygotowanie punktów na terenie dzielnicy Śródmieście oraz wszelkie inne czynności zmierzające do prawidłowego jej przebiegu.
+				</li>
+				<li>
+					Gra została przygotowana w ramach inicjatywy młodzieżowej w związku z realizacją projektu Centrum Aktywności Młodzieżowej dofinansowanego ze środków Unii
+					Europejskiej w ramach regionalnego Programu Operacyjnego Województwa Śląskiego na lata 2014-2020, Działanie 9.1. Aktywna Integracja.
+				</li>
+				<li>Celem gry jest zachęcanie do aktywnego i ciekawego spędzania czasu wolnego.</li>
+				<li>
+					Gra rozpocznie się w dniu 05.11.2021 o godzinie 10:00. Do gry będzie można dołączyć zarówno o godzinie rozpoczęcia, jak i po wyznaczonej godzinie. Gra będzie
+					dostępna przez okres 3 miesięcy.
+				</li>
+				<li>Finał gry w dniu 05.11.2021 będzie się odbywał do godziny 16.30</li>
+				<li>Udział w grze jest bezpłatny.</li>
+				<li>
+					Gra jest przeznaczona dla drużyn złożonych z 1-5 osób. Po trasie gry drużyny poruszają się pieszo, na rowerze, hulajnodze, rolkach itp.. Nie można korzystać z
+					samochodów, skuterów i innych pojazdów silnikowych.
+				</li>
+				<li>
+					Potwierdzenie udziału oraz przejścia gry będzie stanowić informacja podana na stronie internetowej gry po podaniu odpowiedzi na wszystkie zagadki. Kod należy
+					podać obsłudze gry.
+				</li>
+				<li>W dniu premiery tj. 05.11.2021 dla pierwszych 100 osób, które podadzą kod przewidziane są drobne upominki i atrakcje.</li>
+				<li>
+					Uczestnikiem gry musi być osoba pełnoletnia. Osoby niepełnoletnie mogą brać udział w Grze tylko pod opieką osób dorosłych. Jeśli opiekunem osób niepełnoletnich
+					nie jest ich rodzic lub prawny opiekun muszą one posiadać pisemną zgodę rodziców/prawnych opiekunów na udział w Grze. Odpowiednią zgodę należy dostarczyć wraz
+					ze zgłoszeniem lub w dzień Gry do biura CAM pod adresem Kopernika 14 Katowice.
+				</li>
+				<li>
+					Uczestnik gry musi być wyposażony w smartfon z zainstalowana aplikacją do skanowania kodów QR. Kody rozmieszczone są w ścisłym centrum dzielnicy Śródmieście.
+					Znaleziony kod należy zeskanować oraz zastosować się do instrukcji wyświetlonej na ekranie smartfonu (rozwiązać jakieś zadanie lub zagadkę).
+				</li>
+				<li>
+					Wszelkie sytuacje sporne lub nieścisłości mogące powstać w grze należy kierować do Centrum Aktywności Młodzieżowej prowadzonego przez Stowarzyszenie MOST na
+					adres <a href="mailto:cam@mostkatowice.pl">cam@mostkatowice.pl</a>. Od decyzji osób prowadzących nie ma odwołania.
+				</li>
+				<li><p>Uczestnicy Gry powinni być w dobrym stanie zdrowia, umożliwiającym udział w Grze i biorąc udział w grze oświadczają, że:</p></li>
+				<li><p>są świadoma/y zagrożeń wynikających z narażenia na zakażenie wirusem SARS-CoV-2.</p></li>
+				<li>
+					<p>
+						obowiązują się do zakrywania ust i nosa maseczką ochronną lub inną dopuszczalną formą zakrywającą usta i nos oraz do dezynfekcji rąk przy wejściu do budynku,
+						w którym odbywa się finał gry
+					</p>
+				</li>
+				<li>
+					<p>
+						według swojej najlepszej wiedzy:nie stwierdzono u nich zakażenia wirusem SARS–CoV–2 oraz nie wykazują objawów charakterystycznych dla osób zakażonych wirusem
+						SARS-CoV-2, czyli nie mają podwyższonej temperatury, nie kaszlę i nie mają duszności oraz utraty powonienia i smaku;
+					</p>
+				</li>
+				<li>
+					<p>
+						nie zostali poddani żadnej procedurze sanitarnej w związku z zagrożeniem epidemiologicznym związanym z wirusem SARS–CoV–2, nie przebywają na kwarantannie oraz
+						nie są poddani nadzorowi epidemiologicznemu;
+					</p>
+				</li>
+				<li><p>nie mieli styczności z osobą, u której stwierdzono zakażenie wirusem SARS-CoV-2 lub którą poddano kwarantannie.</p></li>
+				<li><p>Uczestnicy Gry są zobowiązani są do przestrzegania aktualnie obowiązujących wytycznych zaleceń i obostrzeń epidemicznych.</p></li>
+				<li>
+					W trakcie Gry zabrania się wykonywania wszelkich czynności mogących spowodować straty materialne. Za powstałe szkody wynikające z działania uczestników mogą oni
+					zostać pociągnięci do odpowiedzialności finansowej i karnej.
+				</li>
+			</ol>
+			<p><strong>Ochrona danych osobowych</strong></p>
+			<p>
+				Zgodnie z art. 13 ust. 1-2 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/79 z dnia 27 kwietnia 2016r., w sprawie ochrony osób fizycznych w związku z
+				przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych) – dalej
+				RODO, informujemy, że:
+			</p>
+			<ol>
+				<li>Administratorem podanych danych osobowych, będzie Stowarzyszenie MOST ul. Bytomska 24 41-800 Zabrze</li>
+				<li><p>Dane osobowe przetwarzane będą w celach i terminach:</p></li>
+				<li>
+					<p>
+						w celu dokumentacji i promocji Gry podczas imprezy mogą być wykonywane zdjęcia lub nagrania wideo, na których może zostać utrwalony wizerunek uczestników.
+						Zdjęcia lub nagrania wideo mogą być następnie wykorzystane i publikowane przez organizatora za pośrednictwem dowolnego medium, w tym w wersji elektronicznej w
+						globalnej sieci WWW (internet), mediach społecznościowych oraz w formie drukowanej. Materiały zbierane są w celu promocji działalności organizatora,
+						upowszechniania kultury oraz w celach archiwizacyjnych. Jeżeli uczestnik nie wyraża zgody na wykonanie i wykorzystanie zdjęć do ww. celów proszony jest o
+						zgłoszenie tego faktu bezpośrednio do Organizatora z chwilą rozpoczęcia gry. W każdym momencie uczestnikowi przysługuje prawo do wniesienia sprzeciwu wobec
+						wykorzystania materiałów z jego wizerunkiem;
+					</p>
+				</li>
+				<li>
+					w celu wypełnienia obowiązków prawnych ciążących na administratorze związanych w szczególności z archiwizacją dokumentacji – na podstawie art. 6 ust. 1 lit c
+					RODO;
+				</li>
+				<li>
+					w celu obrony przed roszczeniami lub dochodzeniem roszczeń (jeżeli takie nastąpią) co jest naszym prawnie uzasadnionym interesem – zgodnie z art. 6 ust. 1 lit.
+					f RODO;
+				</li>
+				<li>
+					<p>
+						oświadczenia o stanie zdrowia uczestnika i domowników zbierane są na wypadek podejrzenia zakażenia wirusem SARS-CoV-2, w takiej sytuacji oświadczenia
+						przekazane będą GIS oraz podległym mu służbom. Dane o stanie zdrowia będą przechowywane przez 2 tygodnie od daty zakończenia wydarzenia, następnie zostaną
+						zniszczone przez Organizatora. Dane przetwarzane będą zgodnie z wytycznymi Ministerstwa Kultury i Dziedzictwa Narodowego oraz Głównego Inspektora Sanitarnego
+						dla dla organizatorów imprez kulturalnych i rozrywkowych w trakcie epidemii wirusa SARS-CoV-2 w Polsce. Wytyczne dostępne są na portalu
+						<a href="http://www.gov.pl/">www.gov.pl</a>
+					</p>
+				</li>
+				<li>
+					<p>
+						W każdej chwili przysługuje Ci prawo do wniesienia sprzeciwu wobec przetwarzania podanych przez Ciebie danych osobowych. Przestaniemy przetwarzać podane przez
+						Ciebie dane w tych celach, chyba, że będziemy w stanie wykazać, że w stosunku do Twoich danych istnieją dla nas ważne prawnie uzasadnione podstawy, które są
+						nadrzędne wobec Twoich interesów, praw i wolności, lub Twoje dane będą nam niezbędne do ewentualnego ustalenia, dochodzenia lub obrony roszczeń.
+					</p>
+				</li>
+				<li><p>Twoje dane nie będą ujawniane innym podmiotom.</p></li>
+				<li><p>Zgodnie z RODO przysługuje Ci:</p></li>
+			</ol>
+			<p>a) Prawo dostępu do podanych danych;</p>
+			<p>b) Prawo do sprostowania (poprawiania) podanych danych;</p>
+			<p>c) Prawo do wniesienia sprzeciwu wobec przetwarzania danych;</p>
+			<p>d) Prawo do wniesienia skargi do organu nadzorczego (PUODO).</p>
+			<ol>
+				<li><p>Nie przysługuje Ci prawo do usunięcia danych o stanie zdrowia oraz prawo do przenoszenia danych.</p></li>
+				<li><p>Informujemy, że podane przez Ciebie dane nie będą wykorzystane do podejmowania decyzji w sposób zautomatyzowany, z uwzględnieniem profilowania.</p></li>
+				<li><p>Podanie danych jest dobrowolne, lecz ich nie podanie będzie skutkowało brakiem możliwości udziału w grze.</p></li>
+			</ol>
+			<p><strong>Postanowienia końcowe</strong></p>
+			<ol>
+				<li>Regulamin Gry znajduje się na stronie …………………………………………..</li>
+				<li>Organizatorzy nie ponoszą odpowiedzialności za zachowanie uczestnikówGry mogące naruszyć porządek publiczny lub dobra osobiste osób trzecich.</li>
+				<li>Organizatorzy nie są stroną między uczestnikami, a osobami trzecimi, których dobra mogą być naruszone w czasie Gry.</li>
+				<li>W kwestiach dotyczących przebiegu Gry, nieprzewidzianych niniejszym regulaminem, głos rozstrzygający mają Organizatorzy.</li>
+				<li>Organizatorzy zastrzegają możliwość odmowy udziału w Grze osobom będącym pod wpływem alkoholu lub innych środków odurzających.</li>
+				<li>Organizatorzy zastrzega sobie możliwość odwołania Gry z uwagi na złe warunki pogodowe uniemożliwiające jej przebieg.</li>
+				<li>Organizatorzy zastrzegają sobie prawo przesunięcia, przedłużenia lub przerwania Gry z ważnych przyczyn.</li>
+				<li>Organizatorzy zastrzegają sobie prawo wprowadzenia zmian w regulaminie.</li>
+			</ol>
+		</article>
 	</n-space>
 </template>
 
@@ -16,4 +159,11 @@
 	});
 </script>
 <style lang="scss" scoped>
+	article {
+		.heading {
+			font-size: 2em;
+		}
+		width: 95%;
+		max-width: 950px;
+	}
 </style>
