0c1cb79c21740dffe310e5c0f48fdc88d89981c3
artalatarta <artur@mucowski.pl>
2021-10-30 11:54:43 +0200

    QR-245: Fix questionHelper in Hacker

--- src/app/poszukiwacze-camienia/routes/codes/Hacker.vue
+++ src/app/poszukiwacze-camienia/routes/codes/Hacker.vue
@@ -1,6 +1,6 @@
 <template>
 	<Video url="https://www.youtube.com/embed/0Gzgvkis0RY" />
-	<p>Wpisz liczbę zauważoną na filmiku. Liczba ta jest przesunięciem do w szyfru cezariańskiego.</p>
+	<p class="questionHelper">Wpisz liczbę zauważoną na filmiku. Liczba ta jest przesunięciem do w szyfru cezariańskiego.</p>
 	<Input label="Przesunięcie" name="displacement" @answer="submitDisplacement($event)" />
 	<Flex v-if="displacement == 13" gap="12px">
 		<p class="questionHelper">Odkoduj słowo przy użyciu szyfru cezariańskiego, pomoże Ci w tym alfabet - górny jest przesunięty, a dolny zwykły</p>
