27bb004a2595545d5f51c8ea464c1b4881f29e07
watjurk <watjurk@gmail.com>
2021-11-04 12:18:15 +0100

    QR-278: Fix map checkpoints names
    Checkpoint names weren't consistent with real codes locations

--- src/app/poszukiwacze-camienia/assets/map.ts
+++ src/app/poszukiwacze-camienia/assets/map.ts
@@ -8,22 +8,22 @@ export const mapData: MapData<puzzleID> = {
 		{
 			localization: [50.255344152043556, 19.01866081183837],
 			name: 'Plac Miarki',
-			puzzleID: 'Archaeologist',
+			puzzleID: 'Treasurer',
 		},
 		{
 			localization: [50.25980242091079, 19.022769820429417],
 			name: 'Teatr Śląski',
-			puzzleID: 'Dancer',
+			puzzleID: 'Hacker',
 		},
 		{
 			localization: [50.259494856483954, 19.013097140674947],
 			name: 'Plac Wolności',
-			puzzleID: 'Hacker',
+			puzzleID: 'Dancer',
 		},
 		{
 			localization: [50.254570216438964, 19.01467819649279],
 			name: 'Kościół Garnizonowy',
-			puzzleID: 'Inspector',
+			puzzleID: 'Archaeologist',
 		},
 		{
 			localization: [50.256722846104275, 19.02100523601609],
@@ -33,7 +33,7 @@ export const mapData: MapData<puzzleID> = {
 		{
 			localization: [50.255995151918356, 19.014963356014082],
 			name: 'Plac Andrzeja',
-			puzzleID: 'Princess',
+			puzzleID: 'Inspector',
 		},
 		{
 			localization: [50.25876354879924, 19.021294856020486],
@@ -43,7 +43,7 @@ export const mapData: MapData<puzzleID> = {
 		{
 			localization: [50.25726038923438, 19.030276456018868],
 			name: 'Kościół Mariacki',
-			puzzleID: 'Treasurer',
+			puzzleID: 'Princess',
 		},
 	],
 };
