bac1560bb89f48c52ae20e645ce814a34080946f
artalatarta <artur@mucowski.pl>
2021-11-04 14:50:52 +0100

    QR-280: Add game info & how to play

---
+++ src/app/poszukiwacze-camienia/routes/AboutGame.vue
@@ -0,0 +1,32 @@
+<template>
+	<Flex>
+		<section>
+			<h2>Więcej o fabule gry</h2>
+			<p>
+				Na ziemię, w samym centrum Katowic spada magiczny artefakt Camień. Posiada on cudowną moc zjednywania przyjaciół, zarażania uśmiechem i dobrą energią. Przez wieki
+				był on poszukiwany nie tylko przez ludzkość ale i istoty pozaziemskie. Teraz masz szansę znaleźć go Ty. W trakcie gry poznasz Kosmitę Ferdynanda, Księżniczkę,
+				Hakera, Inspektor Kluskę i wiele innych postaci, które doprowadzą Cię do rozwiązania zagadki. Przenieś się do świata fantazji i przeżyj z nami tę niesamowitą
+				przygodę.
+			</p>
+			<n-button type="info" @click="$router.push('/')"> Wróć do strony głównej </n-button>
+		</section>
+	</Flex>
+</template>
+
+<script lang="ts">
+	import { NButton } from 'naive-ui';
+	import { defineComponent } from 'vue';
+
+	import Flex from '@/layouts/Flex.vue';
+
+	export default defineComponent({
+		name: 'AboutGame',
+		components: { Flex, NButton },
+	});
+</script>
+<style lang="scss" scoped>
+	section {
+		width: 95%;
+		max-width: 950px;
+	}
+</style>
---
+++ src/app/poszukiwacze-camienia/routes/HowToPlay.vue
@@ -0,0 +1,35 @@
+<template>
+	<Flex>
+		<section>
+			<h2>Jak grać?</h2>
+			<p>
+				"Poszukiwacze CAMienia" to gra terenowa, która pozwala przeżyć niesamowitą przygodę, zachęca do aktywnego spędzenia czasu. Uczestnicy stawiają czoła ukrytym na
+				terenie Katowic- Śródmieścia wskazówkom i za pomocą kodów QR szukają magicznego camienia. Uczestnicy, poruszają się na trasie wyznaczonej kodami QR tj kwadratowy
+				kod graficzny w kształcie kwadratu, na którym znajdują się czarne i białe kwadraty. Kod QR należy zeskanować przy pomocy smartfonu. Do zeskanowania kodu może być
+				potrzebna specjalna aplikacja. Zeskanowanie kodu spowoduje włączenie strony internetowej stworzonej specjalnie do gry – strona ta nie zawiera reklam a jej
+				stosowanie jest bezpieczne. Każdy kodu QR zapewni dostęp do zagadki, której integralną częścią są filmiki. Po rozwiązaniu zagadki pojawi się mapa z
+				umiejscowieniem kolejnego kodu. Do zeskanowania i odnalezienia w jest w sumie 8 kodów umieszczonych w ścisłym centrum dzielnicy Śródmieście. Premiera gry odbędzie
+				się 05.11.2021 o godzinie 10.00. Tego dnia na wszystkich, którzy dotrą do finału czekają atrakcje i nagrody. Więcej informacji w regulaminie
+			</p>
+			<n-button type="info" @click="$router.push('/')"> Wróć do strony głównej </n-button>
+		</section>
+	</Flex>
+</template>
+
+<script lang="ts">
+	import { NButton } from 'naive-ui';
+	import { defineComponent } from 'vue';
+
+	import Flex from '@/layouts/Flex.vue';
+
+	export default defineComponent({
+		name: 'HowToPlay',
+		components: { Flex, NButton },
+	});
+</script>
+<style lang="scss" scoped>
+	section {
+		width: 95%;
+		max-width: 950px;
+	}
+</style>
