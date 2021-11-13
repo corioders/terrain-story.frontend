3d9bd5d79516cb7cac61e45a2c06a64d88a84c11
artalatarta <artur@mucowski.pl>
2021-11-13 11:44:03 +0100

    QR-318: Reroute end to cam

--- src/app/poszukiwacze-camienia/views/End.vue
+++ src/app/poszukiwacze-camienia/views/End.vue
@@ -1,8 +1,8 @@
 <template>
 	<Video videoId="PIJysna39aM" />
-	<p>Dziękujemy za przejście gry! Zapraszamy po odbiór nagrody - znajdź skrytkę i wprowadź kod <span>31140</span></p>
+	<p>Dziękujemy za przejście gry! Zapraszamy po odbiór nagrody do Centrum Aktywności Młodzieżowej - znajdź skrytkę i wprowadź kod <span>31140</span></p>
 	<GMap
-		url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2550.9310696014113!2d19.012753216455202!3d50.25587167944901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce465cc22f79%3A0x96127f5f56c08640!2splac%20Andrzeja%2C%2040-061%20Katowice!5e0!3m2!1sen!2spl!4v1635515719926!5m2!1sen!2spl"
+		url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5101.912886134328!2d19.011423987882665!3d50.25539779028273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce37fbdbaac3%3A0x784bfa3f8e97160e!2sCenter%20NGO%20-%20COP%20Katowice!5e0!3m2!1sen!2spl!4v1636800187485!5m2!1sen!2spl"
 		style="margin: 0"
 	/>
 	<ResetProgressButton />
