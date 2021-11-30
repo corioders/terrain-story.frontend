364eb49c2db417d0f9f8bd5d819c49ae1a8fdc4f
artalatarta <artur@mucowski.pl>
2021-11-30 21:32:49 +0100

    QR-348: Create End mystery

--- src/app/english/views/End.vue
+++ src/app/english/views/End.vue
@@ -1,11 +1,73 @@
 <template>
-	<div>End</div>
+	<VFlex v-if="!displayMark" gap="12px" class="VFlex">
+		<p>Create a sentence from the words.</p>
+		<p>
+			<span v-for="(word, i) in sentence" :key="`word${i}-${word}`" style="margin-right: 3px">{{ word }}</span>
+		</p>
+		<Input label="Sentence" name="sentence" :isCorrect="isSentenceCorrect" @answer="submitInputAnswer($event)" />
+		<SingleChoiceQuestion
+			v-if="isSentenceCorrect"
+			name="What do monkeys sing at Christmas?"
+			question="What do monkeys sing at Christmas?"
+			:options="['Jungle Bells', 'Silent Night', 'Santa Claus is comin\' to town']"
+			answer="Jungle Bells"
+			:displayFeedback="displayFeedback"
+			@correct="handleRadioAnswer(true)"
+			@incorrect="handleRadioAnswer(false)"
+		/>
+		<CheckButton v-if="isSentenceCorrect" :isCorrect="isAnswerCorrect" @click="handleRadioCheck" />
+	</VFlex>
+	<VFlex v-else gap="12px" class="VFlex">
+		<p>Brawo, dostałeś 5</p>
+	</VFlex>
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue';
+	import { defineComponent, ref } from 'vue';
+
+	import Input from '@/components/Input.vue';
+	import CheckButton from '@/components/buttons/CheckButton.vue';
+	import SingleChoiceQuestion from '@/components/closedQuestion/SingleChoiceQuestion.vue';
+	import shuffleOptions from '@/components/closedQuestion/shuffleOptions';
+
+	import { VFlex } from '@corioders/vueui';
 
 	export default defineComponent({
-		name: 'End',
+		name: 'Rebus',
+		components: {
+			Input,
+			SingleChoiceQuestion,
+			CheckButton,
+			VFlex,
+		},
+		setup() {
+			const sentence = shuffleOptions(['What', 'do', 'monkeys', 'sing', 'at', 'Christmas?']);
+
+			let isSentenceCorrect = ref<boolean>(false);
+			function submitInputAnswer(e: string): void {
+				isSentenceCorrect.value = e.toLowerCase() === 'what do monkeys sing at christmas?';
+			}
+
+			const isAnswerCorrect = ref<boolean>(false);
+			const displayFeedback = ref<boolean>(false);
+
+			const handleRadioAnswer = (isSentenceCorrect: boolean): void => {
+				isAnswerCorrect.value = isSentenceCorrect;
+				displayFeedback.value = false;
+			};
+
+			const handleRadioCheck = (): void => {
+				displayFeedback.value = true;
+			};
+
+			const displayMark = ref<boolean>(false);
+
+			return { sentence, isSentenceCorrect, isAnswerCorrect, displayFeedback, displayMark, submitInputAnswer, handleRadioAnswer, handleRadioCheck };
+		},
 	});
 </script>
+<style lang="scss" scoped>
+	.VFlex {
+		max-width: 950px;
+	}
+</style>
