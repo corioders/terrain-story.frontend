48ac832791c34298e50c80a4d0ac184afbf45d56
artalatarta <artur@mucowski.pl>
2021-11-30 20:23:54 +0100

    QR-347: Add placeholder video to Start

--- src/app/english/views/Start.vue
+++ src/app/english/views/Start.vue
@@ -1,11 +1,13 @@
 <template>
 	<div>Start</div>
+	<Video videoId="9hVk_cJyLQM" />
 	<PlayButton @click="handleStart()" />
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue';
 
+	import Video from '@/components/YoutubeVideo.vue';
 	import PlayButton from '@/components/buttons/PlayButton.vue';
 
 	import { useProgressStore } from '@eng/store/progress';
@@ -13,6 +15,7 @@
 	export default defineComponent({
 		name: 'Start',
 		components: {
+			Video,
 			PlayButton,
 		},
 		setup() {
