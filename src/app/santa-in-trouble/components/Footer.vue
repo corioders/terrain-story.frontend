3e1fd844c01e25d30c8325b75dc1af0fd48d3bc8
artalatarta <artur@mucowski.pl>
2021-12-03 19:45:09 +0100

    QR-365: Change angielska to santa in trouble in footer & html

--- src/app/santa-in-trouble/components/Footer.vue
+++ src/app/santa-in-trouble/components/Footer.vue
@@ -5,7 +5,7 @@
 			<p>Projekt dofinansowany ze środków Unii Europejskiej</p>
 		</template>
 		<template #menu>
-			<TinyLink @click="$router.push('/')">Angielska</TinyLink>
+			<TinyLink @click="$router.push('/')">Santa in trouble</TinyLink>
 			<!-- <TinyLink @click="$router.push('/regulamin')">Regulamin</TinyLink> -->
 			<!-- <TinyLink @click="$router.push('/polityka-prywatnosci')">Polityka Prywatności</TinyLink> -->
 			<TinyLink @click="$router.push('/finansowanie')">Finansowanie</TinyLink>
