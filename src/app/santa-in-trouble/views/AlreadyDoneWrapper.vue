07ac3fd29ec516f02fcbd798c77c02ae9cd4793a
artalatarta <artur@mucowski.pl>
2021-12-04 14:11:08 +0100

    QR-363: AlreadyDone fixes

--- src/app/santa-in-trouble/views/AlreadyDoneWrapper.vue
+++ src/app/santa-in-trouble/views/AlreadyDoneWrapper.vue
@@ -1,5 +1,13 @@
 <template>
-	<AlreadyDone :progressStore="progressStore" />
+	<AlreadyDone :progressStore="progressStore">
+		<h2>You've already solved this mystery</h2>
+		<template #showPopupButton>Start playing from the beginning</template>
+		<template #popup>
+			<p>Are you sure you want to start the game from the beginning? <br />This will delete all of the progress in the game.</p>
+		</template>
+		<template #confirmResetButton>Yes</template>
+		<template #cancelResetButton>Cancel</template>
+	</AlreadyDone>
 </template>
 
 <script lang="ts">
