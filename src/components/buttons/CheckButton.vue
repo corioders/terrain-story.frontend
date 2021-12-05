f16a6f4e8f823e68eb5137943e2bd1c1110ea062
artalatarta <artur@mucowski.pl>
2021-12-05 17:19:13 +0100

    QR-381: CheckButton requested fixes

--- src/components/buttons/CheckButton.vue
+++ src/components/buttons/CheckButton.vue
@@ -1,7 +1,5 @@
 <template>
-	<VButton class="checkButton" :class="wrongAnswerState ? 'error' : ''" @click="handleClick($event)">
-		{{ wrongAnswerState ? 'Zła odpowiedź' : 'Sprawdź' }}
-	</VButton>
+	<VButton class="checkButton" :class="wrongAnswerState ? 'error' : ''" @click="handleClick($event)"><div></div></VButton>
 </template>
 
 <script lang="ts">
@@ -24,13 +22,13 @@
 		emits: ['click'],
 		setup(props, { emit }) {
 			let wrongAnswerState = ref(false);
-			const timeout = ref<number | null>(null);
+			let timeout: number | null = null;
 			async function handleClick(e: MouseEvent): Promise<void> {
 				emit('click', e);
-				if (timeout.value !== null) clearTimeout(timeout.value);
+				if (timeout !== null) clearTimeout(timeout);
 				if (!props.isCorrect) {
 					wrongAnswerState.value = true;
-					timeout.value = setTimeout(function () {
+					timeout = setTimeout(function () {
 						wrongAnswerState.value = false;
 					}, 4000);
 				}
@@ -44,8 +42,40 @@
 	.checkButton {
 		@include Button($primary);
 		transition: all 0.5s;
+		div {
+			width: 108px;
+			height: 22px;
+			position: relative;
+			&::before {
+				content: 'Sprawdź';
+				opacity: 1;
+			}
+			&::after {
+				content: 'Zła odpowiedź';
+				opacity: 0;
+			}
+
+			&::before,
+			&::after {
+				width: 100%;
+				top: 0;
+				left: 0;
+				margin: 0 auto;
+				text-align: center;
+				position: absolute;
+				transition: opacity 0.3s ease-in-out;
+			}
+		}
 	}
 	.error {
-		background-color: #{$error};
+		background-color: $error;
+		div {
+			&::before {
+				opacity: 0;
+			}
+			&::after {
+				opacity: 1;
+			}
+		}
 	}
 </style>
