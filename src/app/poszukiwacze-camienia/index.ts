af0cbbc218841b7b47007dd5ece639e96c975b30
watjurk <watjurk@gmail.com>
2021-12-06 11:27:28 +0100

    QR-306: Add vue-gtag-next

--- src/app/poszukiwacze-camienia/index.ts
+++ src/app/poszukiwacze-camienia/index.ts
@@ -1,5 +1,6 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import VueGtagPlugin from 'vue-gtag-next';
 
 import { createLocalStoragePlugin } from '@/store/plugin/localStorage';
 
@@ -9,6 +10,11 @@ import router from './router';
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(createLocalStoragePlugin());
+
+app.use(VueGtagPlugin, {
+	disableScriptLoader: true,
+	property: { id: GTAG_ID },
+});
 
 app.use(pinia).use(router);
 app.mount('#root');
