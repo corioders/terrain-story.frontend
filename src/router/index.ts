d2e3b30fcf210147fee0ddb2f5675dd7eaa4c022
watjurk <watjurk@gmail.com>
2021-12-10 00:24:32 +0100

    QR-387: Move all parameters to url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { trackRouter } from 'vue-gtag-next';
 import { createRouter as createVRouter, createWebHashHistory, RouteLocationRaw, Router, RouteRecordRaw, RouterOptions } from 'vue-router';
 
+import { getRedirectedFromName } from './navigationGuard/progress';
+
 export function createRouter(routes: RouteRecordRaw[], options?: Omit<RouterOptions, 'history' | 'routes'>): Router {
 	const router = createVRouter({
 		routes: routes,
@@ -17,7 +19,7 @@ export function createRouter(routes: RouteRecordRaw[], options?: Omit<RouterOpti
 }
 
 export function navigateToRedirectedFrom(router: Router): void {
-	const redirectedFromName = router.currentRoute.value.params['redirectedFromName'] as string;
+	const redirectedFromName = getRedirectedFromName(router.currentRoute.value.query);
 
 	let location: RouteLocationRaw;
 	if (redirectedFromName !== undefined) location = { name: redirectedFromName };
