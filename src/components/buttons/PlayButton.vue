0fe019d7517b9438bd632df329382052276fcb33
artalatarta <artur@mucowski.pl>
2021-12-17 17:13:02 +0100

    QR-402: Add feedback to playButton when disabled

--- src/components/buttons/PlayButton.vue
+++ src/components/buttons/PlayButton.vue
@@ -1,16 +1,19 @@
 <template>
-	<PrimaryButton :disabled="disabled" @use-disabled-click="$emit('click')">Zagraj</PrimaryButton>
+	<PrimaryButton :disabled="disabled" @click="handleClick" @use-disabled-click="$emit('click')"><slot>Zagraj</slot></PrimaryButton>
+	<VCard v-if="disabled" :style="`opacity:${showCard ? '1' : '0'}`" class="card"><slot name="card">Musisz najpierw zobaczyć film</slot></VCard>
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue';
+	import { defineComponent, ref } from 'vue';
 
 	import { PrimaryButton } from '@/theme/Button';
+	import { VCard } from '@corioders/vueui';
 
 	export default defineComponent({
 		name: 'PlayButton',
 		components: {
 			PrimaryButton,
+			VCard,
 		},
 		props: {
 			disabled: {
@@ -20,5 +23,21 @@
 			},
 		},
 		emits: ['click'],
+		setup(props) {
+			const showCard = ref(false);
+			function handleClick(): void {
+				if (props.disabled === false) return;
+				showCard.value = true;
+				setTimeout(() => {
+					showCard.value = false;
+				}, 1000);
+			}
+			return { showCard, handleClick };
+		},
 	});
 </script>
+<style lang="scss" scoped>
+	.card {
+		transition: opacity 0.2s ease-in-out;
+	}
+</style>
