324340106278416e672885b7229ed6d1b15139df
artalatarta <artur@mucowski.pl>
2021-12-17 21:06:57 +0100

    QR-415: Create FloorMap component

--- src/components/map/floor/FloorMap.vue
+++ src/components/map/floor/FloorMap.vue
@@ -1,14 +1,27 @@
 <template>
-	<div></div>
+	<VFlex class="container" gap="12px">
+		<div v-for="(floor, i) in floorMapDescriptor.floors" :key="`floor${i}-${floor}`" class="floor">
+			<VFlex direction="row" justify="center" gap="12px">
+				<div v-for="(puzzleID, j) in floor.puzzles" :key="`puzzle${j}-${puzzleID}`" class="puzzle" :class="puzzlesDone[puzzleID] === true ? 'done' : ''" />
+			</VFlex>
+			<div class="floor --line" />
+			<p class="floor --label">{{ floor.name }}</p>
+		</div>
+	</VFlex>
 </template>
 
 <script lang="ts">
 	import { defineComponent, PropType } from 'vue';
 
+	import { VFlex } from '@corioders/vueui';
+
 	import { FloorMapDescriptor } from './floorMap';
 
 	export default defineComponent({
 		name: 'FloorMap',
+		components: {
+			VFlex,
+		},
 		props: {
 			floorMapDescriptor: {
 				type: Object as PropType<FloorMapDescriptor<string>>,
@@ -19,18 +32,36 @@
 				required: true,
 			},
 		},
-		setup(props) {
-			for (const floor of props.floorMapDescriptor.floors) {
-				for (const puzzleID of floor.puzzles) {
-					if (props.puzzlesDone[puzzleID] === true) {
-						// Do something when puzzle is done
-					} else {
-						// Do something when puzzle isn't  done
-						console.log(puzzleID);
-					}
-				}
-			}
-		},
 	});
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	$offset: 4px;
+	.container {
+		margin: 50px 0;
+		min-width: 300px;
+		max-width: 100%;
+		.floor {
+			width: 100%;
+			.puzzle {
+				width: 25px;
+				height: 25px;
+				background-color: $disabled;
+				border-radius: 50%;
+				margin-bottom: $offset;
+				&.done {
+					background-color: $primary;
+				}
+			}
+			&.--line {
+				width: 100%;
+				height: $offset;
+				border-radius: 5px;
+				background-color: $infoDarker;
+			}
+			&.--label {
+				margin: 0 0 0 $offset;
+				font-size: 0.8em;
+			}
+		}
+	}
+</style>
