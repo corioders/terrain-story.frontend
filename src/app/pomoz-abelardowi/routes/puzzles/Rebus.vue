a6ea5a08d401ef562656fd03033b5fb8faf1a13b
artalatarta <artur@mucowski.pl>
2022-01-21 14:26:59 +0100

    QR-431: Create Rebus puzzle

--- src/app/pomoz-abelardowi/routes/puzzles/Rebus.vue
+++ src/app/pomoz-abelardowi/routes/puzzles/Rebus.vue
@@ -1,1 +1,45 @@
-<template>Rebus</template>
+<template>
+	<p>Co jest najważniejsze w każdej relacji? Rozwiąż rebus, aby odnaleźć hasło.</p>
+	<img src="@help/assets/rebus.webp" alt="Rebus: money without 'ey' + key + s" />
+	<VInput v-model="answer" class="VInput" name="Odpowiedź" @input="submitAnswer" />
+
+	<CheckButton :isCorrect="isAnswerCorrect" @click="handleCheck()" />
+</template>
+
+<script lang="ts">
+	import { defineComponent, ref } from 'vue';
+
+	import CheckButton from '@/components/buttons/CheckButton.vue';
+
+	import { VInput } from '@corioders/vueui';
+	import { useProgressStore } from '@help/store/progress';
+
+	export default defineComponent({
+		name: 'Rebus',
+		components: {
+			VInput,
+			CheckButton,
+		},
+		setup() {
+			const store = useProgressStore();
+
+			let isAnswerCorrect = ref(false);
+			let answer = ref('');
+			function submitAnswer(): void {
+				isAnswerCorrect.value = answer.value.toLowerCase() === 'zaufanie';
+			}
+
+			const handleCheck = (): void => {
+				if (isAnswerCorrect.value === true) store.finishPuzzle('Rebus');
+			};
+
+			return { answer, submitAnswer, isAnswerCorrect, handleCheck };
+		},
+	});
+</script>
+<style lang="scss" scoped>
+	img {
+		width: 100%;
+		max-width: 950px;
+	}
+</style>
