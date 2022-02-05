f4f7bf982484e6c77470a8a20a11d85d482742e4
artalatarta <artur@mucowski.pl>
2022-02-05 08:08:24 +0100

    QR-455: Assign puzzles to locations in help game

--- src/app/pomoz-abelardowi/assets/map/leaflet/kat/index.ts
+++ src/app/pomoz-abelardowi/assets/map/leaflet/kat/index.ts
@@ -2,7 +2,6 @@ import { MapData } from '@/components/map/leaflet/map';
 
 import { PuzzleID } from '@help/store/progress';
 
-//TODO: UPDATE puzzleID
 export const mapData: MapData<PuzzleID> = {
 	center: [50.25876354879924, 19.021294856020486],
 	zoom: 14,
@@ -16,43 +15,43 @@ export const mapData: MapData<PuzzleID> = {
 		{
 			localization: [50.25972846105917, 19.02250058578311],
 			name: 'Teatr Śląski',
-			puzzleID: 'Bingo',
+			puzzleID: 'Emotions1',
 			photo: require<string>('./teatrSlaski.webp'),
 		},
 		{
 			localization: [50.25972891670551, 19.013331369366664],
 			name: 'Plac Wolności',
-			puzzleID: 'Bingo',
+			puzzleID: 'Emotions2',
 			photo: require<string>('./placWolnosci.webp'),
 		},
 		{
 			localization: [50.25466308086385, 19.01492298980046],
 			name: 'Kościół Garnizonowy',
-			puzzleID: 'Bingo',
+			puzzleID: 'Jacobson',
 			photo: require<string>('./kosciolGarnizonowy.webp'),
 		},
 		{
 			localization: [50.255481545175385, 19.018445075626712],
 			name: 'Faza Charity Shop',
-			puzzleID: 'Bingo',
+			puzzleID: 'Matching1',
 			photo: require<string>('./fazaCharityShop.webp'),
 		},
 		{
 			localization: [50.25556372937672, 19.014722451529728],
 			name: 'Plac Andrzeja',
-			puzzleID: 'Bingo',
+			puzzleID: 'Matching2',
 			photo: require<string>('./placAndrzeja.webp'),
 		},
 		{
 			localization: [50.25861903580001, 19.021386861479904],
 			name: 'Centrum Informacji Turystycznej',
-			puzzleID: 'Bingo',
+			puzzleID: 'PhoneNumbers',
 			photo: require<string>('./centrumInformacjiTurystycznej.webp'),
 		},
 		{
 			localization: [50.2571646958035, 19.0299658008134],
 			name: 'Kościół Mariacki',
-			puzzleID: 'Bingo',
+			puzzleID: 'Rebus',
 			photo: require<string>('./kosciolMariacki.webp'),
 		},
 	],
