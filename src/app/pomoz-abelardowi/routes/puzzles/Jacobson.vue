1aaa2390a1a7ca1fe893cecd4718ee28f77f8029
artalatarta <artur@mucowski.pl>
2022-03-09 13:45:12 +0100

    QR-522: Improve Jacobson

--- src/app/pomoz-abelardowi/routes/puzzles/Jacobson.vue
+++ src/app/pomoz-abelardowi/routes/puzzles/Jacobson.vue
@@ -4,29 +4,13 @@
 		się.
 	</p>
 
-	<ol>
-		<li>Weź dwa głębokie oddechy. Powietrze staraj się wydychać powoli.</li>
-		<li>Zaciśnij pięści obu rąk. Poczuj napięcie. Przytrzymaj tak około 5 sekund… i odpuść… Poczuj, jak Twoje dłonie przyjemnie się odprężają.</li>
-		<li>Teraz napnij wszystkie mięśnie obu rąk, przyciskając przedramiona do swoich barków. Przytrzymaj… i odpuść…</li>
-		<li>Podnieś swoje barki jak najwyżej — tak jakbyś wzruszał ramionami. Przytrzymaj… odpuść…</li>
-		<li>Teraz zamknij oczy, zaciśnij powieki i napnij wszystkie mięśnie wokół oczu. Zaciśnij mocno… i puść…</li>
-		<li>Zaciśnij zęby i jednocześnie mocno przyciśnij język do podniebienia. Przytrzymaj… odpuść…</li>
-		<li>
-			Przesuń głowę do tyłu i napnij tylne mięśnie swojej szyi. Przytrzymaj… odpuść… Ten punkt powtórz dwa razy, ponieważ mięśnie szyi są zwykle najbardziej napięte.
-		</li>
-		<li>Cofnij ramiona tak, aby napiąć mięśnie swoich pleców, naprężając je jakby w łuk. Przytrzymaj… rozluźnij…</li>
-		<li>Wciągnij brzuch, tak jakbyś chciał przycisnąć go do kręgosłupa. Przytrzymaj… odpuść…</li>
-		<li>Wyprostuj nogi i ściągnij uda. Napnij mięśnie. Przytrzymaj… rozluźnij…</li>
-		<li>Podnieś stopy z ziemi i kierując palce stóp jak najbardziej w kierunku swojej głowy, napnij mięśnie stóp. Przytrzymaj… odpuść…</li>
-		<li>Podnieś stopy jeszcze raz, tym razem podkurczając palce tak, jakbyś chciał je zagrzebać w piasku. Napnij mięśnie stóp. Przytrzymaj… odpuść…</li>
-		<li>Kiedy uznasz, że ćwiczenie dobiegło końca, weź trzy głębokie oddechy.</li>
-	</ol>
-	<PrimaryButton :disabled="disabled" @click="handleClick">Zrobione</PrimaryButton>
+	<p>{{ jacobson[index] }}</p>
+	<PrimaryButton @click="handleClick">{{ index < jacobson.length - 1 ? 'Dalej' : 'Zrobione' }}</PrimaryButton>
 </template>
 
 <script lang="ts">
 import { useProgressStore } from '@help/store/progress';
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import { PrimaryButton } from '@/theme/Button';
 
@@ -36,18 +20,30 @@ export default defineComponent({
 		PrimaryButton,
 	},
 	setup() {
-		const disabled = ref(true);
-		onMounted(() => {
-			setTimeout(() => {
-				disabled.value = false;
-			}, 3000);
-		});
+		const jacobson = [
+			'Weź dwa głębokie oddechy. Powietrze staraj się wydychać powoli.',
+			'Zaciśnij pięści obu rąk. Poczuj napięcie. Przytrzymaj tak około 5 sekund… i odpuść… Poczuj, jak Twoje dłonie przyjemnie się odprężają.',
+			'Teraz napnij wszystkie mięśnie obu rąk, przyciskając przedramiona do swoich barków. Przytrzymaj… i odpuść…',
+			'Podnieś swoje barki jak najwyżej — tak jakbyś wzruszał ramionami. Przytrzymaj… odpuść…',
+			'Teraz zamknij oczy, zaciśnij powieki i napnij wszystkie mięśnie wokół oczu. Zaciśnij mocno… i puść…',
+			'Zaciśnij zęby i jednocześnie mocno przyciśnij język do podniebienia. Przytrzymaj… odpuść…',
+			'Przesuń głowę do tyłu i napnij tylne mięśnie swojej szyi. Przytrzymaj… odpuść… Ten punkt powtórz dwa razy, ponieważ mięśnie szyi są zwykle najbardziej napięte.',
+			'Cofnij ramiona tak, aby napiąć mięśnie swoich pleców, naprężając je jakby w łuk. Przytrzymaj… rozluźnij…',
+			'Wciągnij brzuch, tak jakbyś chciał przycisnąć go do kręgosłupa. Przytrzymaj… odpuść…',
+			'Wyprostuj nogi i ściągnij uda. Napnij mięśnie. Przytrzymaj… rozluźnij…',
+			'Podnieś stopy z ziemi i kierując palce stóp jak najbardziej w kierunku swojej głowy, napnij mięśnie stóp. Przytrzymaj… odpuść…',
+			'Podnieś stopy jeszcze raz, tym razem podkurczając palce tak, jakbyś chciał je zagrzebać w piasku. Napnij mięśnie stóp. Przytrzymaj… odpuść…',
+			'Kiedy uznasz, że ćwiczenie dobiegło końca, weź trzy głębokie oddechy.',
+		];
+
+		const index = ref(0);
 
 		function handleClick(): void {
-			if (disabled.value) return;
-			useProgressStore().finishPuzzle('Jacobson');
+			if (index.value < jacobson.length - 1) index.value++;
+			else useProgressStore().finishPuzzle('Jacobson');
 		}
-		return { disabled, handleClick };
+
+		return { jacobson, index, handleClick };
 	},
 });
 </script>
