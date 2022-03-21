5b5d6e556590d836830269a0f2df85d44f091ee6
watjurk <watjurk@gmail.com>
2022-03-21 21:11:26 +0100

    QR-558: Move home to shared component

--- src/app/pomoz-abelardowi/routes/Home.vue
+++ src/app/pomoz-abelardowi/routes/Home.vue
@@ -1,155 +1,37 @@
 <template>
 	<DevNav v-if="!IS_PRODUCTION" :routes="routes" />
-	<VFlex class="VFlex">
-		<div v-once>
-			<img
-				v-if="window.width.value * 1.5 >= window.height.value"
-				class="gameLogo"
-				src="@help/assets/helpLogo.webp"
-				alt="Pomóż Abelardowi"
-				role="heading"
-				aria-level="2"
-			/>
-			<img v-else class="gameLogo" src="@help/assets/helpLogoHorizontal.webp" alt="Pomóż Abelardowi" role="heading" aria-level="2" />
-		</div>
-		<section class="dark">
-			<VFlex>
-				<h3>Jak grać?</h3>
-				<Video class="video" videoId="osnMvAcWdZc" />
-			</VFlex>
-		</section>
-		<section class="light">
-			<VFlex>
-				<h3>Chcesz wiedzieć więcej?</h3>
-				<Video class="video" videoId="yLmjRKen2UE" />
-			</VFlex>
-		</section>
-		<section class="dark">
-			<VFlex>
-				<h5 style="margin-bottom: 24px">Szukasz naszych gier?</h5>
-				<LocationMapAllWrapper />
-			</VFlex>
-		</section>
-		<section class="light">
-			<VFlex>
-				<h6 style="margin: 0">Kontakt</h6>
-				<p style="margin-bottom: 0">terrainstory@gmail.com</p>
-				<p style="margin-top: 0">
-					Wiadomość na <InfoLink href="https://www.instagram.com/terrain.story/" newCard>Instagramie</InfoLink> lub
-					<InfoLink href="https://www.facebook.com/terrain.story/" newCard>Facebooku</InfoLink>
-				</p>
-			</VFlex>
-		</section>
-		<section class="dark">
-			<VFlex>
-				<h5>Twórcy Gry</h5>
-				<img class="logo" src="@/assets/whiteLogo.webp" alt="Terrain Story" />
-				<p>Psycholog Łucja Chwastek</p>
-			</VFlex>
-		</section>
-		<section class="light">
-			<VFlex>
-				<h6>Honorowe Patronaty</h6>
-				<img src="@/assets/patronage/silesia.webp" alt="Marszałek Województwa Śląskiego" class="logoVertical" />
-				<img src="@/assets/patronage/katowice.webp" alt="Prezydent Miasta Katowice" class="logoVertical" />
-				<h6>Patroni medialni</h6>
-				<img class="logo logoLink" src="@/assets/patrons/portalKatowice.webp" alt="Portal Katowice" @click="redirect" />
-			</VFlex>
-		</section>
-	</VFlex>
+	<Home
+		gameName="Pomóż Abelardowi"
+		:gameLogoSrc="require('@help/assets/helpLogo.webp')"
+		:gameLogoHorizontalSrc="require('@help/assets/helpLogoHorizontal.webp')"
+		knowMoreVideoId="yLmjRKen2UE"
+		lightSectionColor="#406258"
+		:isTextLightTheme="true"
+	>
+		<template #gameCreators>
+			<p>Psycholog Łucja Chwastek</p>
+		</template>
+	</Home>
 </template>
 
 <script lang="ts">
-import { VFlex } from '@corioders/vueui';
 import { routes } from '@help/router';
-import { useWindowSize } from '@vueuse/core';
 import { defineComponent } from 'vue';
 
-import LocationMapAllWrapper from '@/components/LocationMapAllWrapper.vue';
-import Video from '@/components/YoutubeVideo.vue';
+import Home from '@/components/Home.vue';
 import DevNav from '@/components/devHelpers/DevNav.vue';
-import { InfoLink } from '@/theme/Link';
 
 export default defineComponent({
-	name: 'Home',
+	name: 'HomeWrapper',
 	components: {
 		DevNav,
-		VFlex,
-		Video,
-		LocationMapAllWrapper,
-		InfoLink,
+		Home,
 	},
 	setup() {
-		function redirect(): void {
-			location.href = 'http://www.portal.katowice.pl';
-		}
-		return { routes, IS_PRODUCTION: __IS_PRODUCTION__, window: useWindowSize(), redirect };
+		return { routes, IS_PRODUCTION: __IS_PRODUCTION__ };
 	},
 });
 </script>
-<style lang="scss" scoped>
-.VFlex {
-	width: 100%;
-
-	.gameLogo {
-		width: 100%;
-		max-width: 1500px;
-		margin-bottom: 64px;
-	}
-
-	section {
-		width: 100%;
-		padding: 64px 8px;
-		text-align: center;
-
-		h3,
-		h4,
-		h5,
-		h6 {
-			font-weight: 600;
-			font-size: 2em;
-			margin-bottom: 0;
-		}
-
-		p {
-			font-size: 18px;
-			max-width: 950px;
-		}
-
-		.logo,
-		%logo {
-			width: 200px;
-			max-height: 150px;
-			object-fit: contain;
-			margin: 18px 0;
-		}
-
-		.logoVertical {
-			@extend %logo;
-
-			width: 100vw;
-			max-height: 200px;
-
-			@media (min-width: 500px) {
-				width: 500px;
-			}
-		}
-
-		.video {
-			margin-top: 24px;
-		}
-	}
-}
-
-.dark {
-	background-color: #406258;
-	color: #fff;
-}
-
-.logoLink {
-	cursor: pointer;
-}
-</style>
 <style lang="scss">
 body {
 	margin: 0;
